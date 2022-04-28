69f26d5b816a702ac183bdb5a14e13d6d6f81742
sacOO7 <sachinshinde7676@gmail.com>
2022-04-28 22:21:17 +0530

    Updated bootstrap file, added ably config.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,9 @@
 window._ = require('lodash');
 
+try {
+    require('bootstrap');
+} catch (e) {}
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -16,13 +20,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from 'laravel-echo';
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '',
+    wsHost: 'realtime-pusher.ably.io',
+    wsPort: 443,
+    disableStats: false,
+    encrypted: true,
+});
