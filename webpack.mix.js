7812368623b3ac73c34125e9a8e4cc8574386338
sacOO7 <sachinshinde7676@gmail.com>
2022-06-22 12:42:45 +0530

    Added webpack config. to the mix, excluding http and https polyfills

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
+ | for your Laravel application. By default, we ae compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
@@ -14,3 +14,12 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.webpackConfig({
+        resolve: {
+            fallback: {
+                "http": false,
+                "https": false,
+            },
+        }
+})
