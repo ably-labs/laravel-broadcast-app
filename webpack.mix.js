450cb377e07e3f06acae65ad5b090a8ffd557e3e
QSD_stefan <info@qsdbih.com>
2022-07-13 16:18:24 +0200

    Generate sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .sourceMaps();
 
 mix.webpackConfig({
         resolve: {
