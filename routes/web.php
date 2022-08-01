9b4d95012cbc73cefcf25921e34a2d2ad38566a3
QSD_stefan <info@qsdbih.com>
2022-08-01 18:25:00 +0200

    Fix param order in web.php routes

--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,15 @@ Route::get('/', function () {
 });
 
 Route::get('/public-event', function (Request $request) {
-    return event(new PublicMessageNotification($request->message, $request->channel));
+    return event(new PublicMessageNotification($request->channel, $request->message));
 });
 
 Route::get('/private-event', function (Request $request) {
-    return event(new PrivateMessageNotification($request->message, $request->channel));
+    return event(new PrivateMessageNotification($request->channel, $request->message));
 });
 
 Route::get('/presence-event', function (Request $request) {
-    return event(new PresenceMessageNotification($request->message, $request->channel));
+    return event(new PresenceMessageNotification($request->channel, $request->message));
 });
 
 Route::get('/listen', function () {
