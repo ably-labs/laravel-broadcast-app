86181e0eeddb9aeabf8b85d4a508e04cc720b906
QSD_stefan <info@qsdbih.com>
2022-08-01 19:14:01 +0200

    Comment out the notification private channel, to serve as an example for documentation

--- routes/channels.php
+++ routes/channels.php
@@ -14,9 +14,14 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
+/*
+ * Register a private channel, this is not necessary for public channels
+ * The return value determines whether the user is authorized to join this private channel
+
 Broadcast::channel('notification', function ($user) {
     return true;
 });
+*/
 
 Broadcast::channel('room-{roomId}', function ( User $user, $roomId) { // for presence channel return data about user -> https://laravel.com/docs/9.x/broadcasting#authorizing-presence-channels
     if($user->canJoinRoom($roomId))
