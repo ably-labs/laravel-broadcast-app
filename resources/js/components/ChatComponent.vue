80ff1609dccac27633374749f08e80a1360ef053
QSD_stefan <info@qsdbih.com>
2022-08-02 13:28:07 +0200

    Add Pusher-compatible error handling to joinPrivate() error callback

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -136,16 +136,14 @@ export default {
                         this.pushPublicNotification(channel, data)
                 })
                 .error((err) => {
-                    if('statusCode' in err) {
-                        if(err.statusCode === 403) {
-                            if(!window.authUser)
-                                alert("You don't have the access to join this private channel, try logging into the application.");
-                            else
-                                alert("You don't have the access to join this private channel, try entering the channel room-1 or room-2");
-                        }
+                    if((typeof err === 'object' && 'statusCode' in err && err.statusCode === 403) || err === 403) {
+                        if(!window.authUser)
+                            alert("You don't have the access to join this private channel, try logging into the application.");
                         else
-                            alert("An error occurred while trying to join a private channel, check the console for details.");
+                            alert("You don't have the access to join this private channel, try entering the channel room-1 or room-2");
                     }
+                    else
+                        alert("An error occurred while trying to join a private channel, check the console for details.");
 
                     console.error(err);
                 });
