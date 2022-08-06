0ce941f6b17feb809ce36531e31af637d8ffae35
QSD_stefan <info@qsdbih.com>
2022-08-06 06:08:15 +0200

    Add MessageComponent

--- resources/js/components/ChatComponent.vue
+++ resources/js/components/ChatComponent.vue
@@ -15,13 +15,7 @@
                  :id="'tab-content-' + channel.formattedName"
                  :key="'tab-content-' + channel.formattedName">
                 <div class="messageContainer">
-                    <div v-for="msg in channel.messages">
-                        <span v-if="msg.time instanceof Date">[{{ msg.time.toLocaleTimeString() }}]</span>
-                        <span v-if="msg.type === 'status'"><i>{{ msg.content }}</i></span>
-                        <span v-else-if="msg.user"><b>{{ msg.user }}</b>: {{msg.content}}</span>
-                        <span v-else-if="msg.type === 'broadcast'"><b>{{ msg.content }}</b></span>
-                        <span v-else>{{ msg }}</span>
-                    </div>
+                    <message-component v-for="msg in channel.messages" :key="msg.time.getTime() + '-' + msg.content" :msg="msg"/>
                 </div>
 
                 <div v-if="channel.type === 'private'">
