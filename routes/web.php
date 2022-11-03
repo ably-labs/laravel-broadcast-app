b2db2216fd0c82d0aba8c485415b28ee6a7d1496
QSD_amir <info@qsdbih.com>
2022-11-03 18:22:15 +0100

    Redirect from website root to listen demo page

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('listen');
 });
 
 Route::get('/public-event', function (Request $request) {
