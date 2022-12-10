ad2f1d8228ba9e770893cd87642fa809bf4004f3
sacOO7 <sachinshinde7676@gmail.com>
2022-12-10 20:31:53 +0530

    Added important documentation note to laravel api routes

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Important Note -
+// Publishing message through laravel to ably allows request throttling, and filtering + persistent storage for messages.
+// Public channel allows to publish messages only through laravel to ably.
+// Private/Presence channels allows to send/publish messages in two ways 
+// 1. Through laravel to ably [ client1 -> laravel -> ably -> client2]
+// 2. Through client-events [ client1 -> ably -> client2]
+// Publishing via client-events is faster than through laravel, since there is no intermediate party involved.
+// Read more about client-events => https://laravel.com/docs/broadcasting#client-events
+
 // Public broadcast for a guest user.
-// Throttle to 60 requests per 1 minute per machine.
-// This is generally to prevent DOS/DDOS/message spamming on public channels.
+// Throttle to 60 requests/1 minute per machine.
+// This is generally to prevent DOS attack/message spamming on public channels.
 // https://dev.to/aliadhillon/new-simple-way-of-creating-custom-rate-limiters-in-laravel-8-65n.
 Route::middleware('throttle:60,1')->get('/public-event', function (Request $request) {
     broadcast(new PublicMessageEvent($request->channel, $request->message));
 });
 
-// Private broadcast for a authenticated user.
-// This alternative option to send messages is slow compared to client-events. 
-// since it goes through laravel before being sent to ably.
-// This allows message filtering, throttling and persistent storage before being sent.
+// Private broadcast for an authenticated user.
 Route::get('/private-event', function (Request $request) {
     if($request->input('to_others')) {
         broadcast(new PrivateMessageEvent($request->channel, $request->message))->toOthers();
